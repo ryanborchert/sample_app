760b69563bbacc19a19da78f6dfd4f8fcb29ffec
RyanBorchert <ryan.borchert@gmail.com>
2012-09-23 15:09:39 -0400

    Added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,24 @@ require 'spec_helper'
 
 describe "Static pages" do
 	describe "Home page" do
-		it "shoould have the content 'Sample App'" do
+		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the right title" do
+		it "should have the base title" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
-							:test => "Ruby on Rails Tutorial Sample App | Home")						
+							:test => "Ruby on Rails Tutorial Sample App")						
 							
 		end		
+		
+		it "should have a custom page title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', 
+							:test => '| Home')						
+							
+		end				
 	end
 	
 	describe 'Help page' do
@@ -39,6 +46,20 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
 							:test => "Ruby on Rails Tutorial Sample App | About Us")						
+							
+		end				
+	end	
+
+	describe 'Contact page' do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+							:test => "Ruby on Rails Tutorial Sample App | Contact")						
 							
 		end				
 	end	
